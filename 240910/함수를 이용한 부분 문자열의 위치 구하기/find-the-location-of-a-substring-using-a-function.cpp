0da71156e988ce6a58ac18d6882f964464bc79e3
sindongmyung <41135138+sdm6410@users.noreply.github.com>
2024-09-10 13:43:16 +0900

    Title: 함수를 이용한 부분 문자열의 위치 구하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240910/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.cpp
+++ 240910/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.cpp
@@ -7,6 +7,12 @@ int main() {
     string c;
     cin >> str;
     cin >> c;
-    cout << str.find(c);
+    int n = str.find(c);
+    if(n > 1005)
+        cout << "-1";
+    else
+    {
+        cout << n;
+    }
     return 0;
 }
