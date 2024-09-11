565e0631f13aaab6563b11e67851d39d221d9773
sindongmyung <41135138+sdm6410@users.noreply.github.com>
2024-09-11 17:14:49 +0900

    Title: 큰 숫자 자리수의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240911/큰 숫자 자리수의 합/sum-of-large-numeric-digits.cpp
+++ 240911/큰 숫자 자리수의 합/sum-of-large-numeric-digits.cpp
@@ -2,17 +2,16 @@
 #include <string>
 using namespace std;
 
+int DigitSum(int n)
+{
+    if(n < 10)
+        return n;
+    return DigitSum(n / 10) + n % 10;
+}
+
 int main() {
     int a, b, c;
-    int prob;
     cin >> a >> b >> c;
-    prob = a * b * c;
-    string str = to_string(prob);
-    int sum = 0;
-    for(int i = 0; i < str.length(); i++)
-    {
-        sum += str[i] - '0';
-    }
-    cout << sum;
+    cout << DigitSum(a * b * c);
     return 0;
 }
