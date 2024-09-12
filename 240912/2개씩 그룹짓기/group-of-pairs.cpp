53b33398c03635e1169288ab261bd0f730c1f66e
sindongmyung <41135138+sdm6410@users.noreply.github.com>
2024-09-12 10:07:33 +0900

    Title: 2개씩 그룹짓기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240912/2개씩 그룹짓기/group-of-pairs.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+void SortArray(int *arr1, int *arr2, int n)
+{
+    sort(arr1, arr1 + n * 2);
+    sort(arr2, arr2 + n * 2, greater<int>());
+    int max = arr1[0];
+    int sum = 0;
+    for(int i = 0; i < n* 2; i++)
+    {
+        sum = arr1[i] + arr2[i];
+        if(max < sum)
+        {
+            max = sum;
+        }
+    }
+    if(n == 1)
+    {
+        cout << arr1[0];
+    }
+    else
+    {
+        cout << max;
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+    int arr1[n * 2];
+    int arr2[n * 2];
+    for(int i = 0; i < n * 2; i++)
+    {  
+        cin >> arr1[i];
+        arr2[i] = arr1[i];
+    }
+    
+    SortArray(arr1, arr2, n);
+    
+    
+    return 0;
+}
