4dd13a75677c73c0e1eb965d6294ccd030c4ad33
sindongmyung <41135138+sdm6410@users.noreply.github.com>
2024-09-12 12:10:40 +0900

    Title: 상품 코드, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240912/상품 코드/product-code.cpp
+++ 240912/상품 코드/product-code.cpp
@@ -15,7 +15,7 @@ int main() {
     Product p;
     cout << "product " << p.code << " is " << p.name << endl;
     string str;
-    int num
+    int num;
     cin >> str >> num;
     Product p1 = Product(str, num);
     cout << "product " << p1.code << " is " << p1.name;
