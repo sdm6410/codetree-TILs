1fac019e37097b69742917d8ae03e3ec9f3ad646
sindongmyung <41135138+sdm6410@users.noreply.github.com>
2024-09-12 12:17:04 +0900

    Title: 사는 지역, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240912/사는 지역/where-live.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class HumInfo
+{
+    public:
+        string name;
+        string address;
+        string location;
+    HumInfo()
+    {
+
+    }
+
+    HumInfo(string name, string address, string location)
+    {
+        this->name = name;
+        this->address = address;
+        this->location = location;
+    }
+};
+
+
+int main() {
+    int n;
+    cin >> n;
+    HumInfo humInfo[n];
+    for(int i = 0; i < n; i++)
+    {
+        string name;
+        string address;
+        string location;
+        cin >> name >> address >> location;
+        humInfo[i] = HumInfo(name, address, location);
+    }
+
+    cout << "name "<< humInfo[n -1].name <<endl;
+    cout << "addr "<< humInfo[n -1].address <<endl;
+    cout << "city "<< humInfo[n -1].location <<endl;
+
+    return 0;
+}
