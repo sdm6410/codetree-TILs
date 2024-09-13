223117062ae529ef71773ff30da709a125ddd53d
sindongmyung <41135138+sdm6410@users.noreply.github.com>
2024-09-13 16:10:24 +0900

    Title: Date to Date, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240913/Date to Date/date-to-date.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int month1, day1, month2, day2;
+    int num_of_days[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    cin >> month1 >> day1 >> month2 >> day2;
+    int sum = 0;
+    for(int i = month1 + 1; i <= month2; i++)
+    {
+        sum += num_of_days[i];
+    }
+    sum = sum + day2 - day1;
+    cout << sum;
+    return 0;
+}
