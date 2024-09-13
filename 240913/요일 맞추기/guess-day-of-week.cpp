38f664620fba97daa1c0dffa022d8581ba38a25a
sindongmyung <41135138+sdm6410@users.noreply.github.com>
2024-09-13 17:38:22 +0900

    Title: 요일 맞추기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240913/요일 맞추기/guess-day-of-week.cpp
+++ 240913/요일 맞추기/guess-day-of-week.cpp
@@ -6,20 +6,23 @@ int main() {
     int num_of_days[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
     cin >> m1 >> d1 >> m2 >> d2;
     string day[7] = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
-    int n = (m2 - m1) * 30 + d2 - d1;
+    int sum = 1;
+    for(int i = month1; i < month2; i++)
+    {
+        sum += num_of_days[i-1];
+    }
+    sum += d2;
+    sum -= d1;
     if(n < 0)
     {
         while(n < 0)
         {
-            n += 7;
+            sum += 7;
         }
     }
     else
     {
-        while(n > 6)
-        {
-            n -= 7;
-        }
+        sum %= 7;
     }
 
 
