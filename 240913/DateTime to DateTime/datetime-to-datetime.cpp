d2cfe916f47919ba21fa69c0e3cf6aedd6316a0c
sindongmyung <41135138+sdm6410@users.noreply.github.com>
2024-09-13 14:52:05 +0900

    Title: DateTime to DateTime, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240913/DateTime to DateTime/datetime-to-datetime.cpp
+++ 240913/DateTime to DateTime/datetime-to-datetime.cpp
@@ -5,32 +5,14 @@ int main() {
     int a1, b1, c1;
     cin >> a1 >> b1 >> c1;
     int a2 = 11, b2 = 11, c2 = 11;
-    int elapsed_time = 0;
-    if(a1 < a2 || (a1 < a2 && b1 < b2 && c1 < c2))
+    int totalTime = (a1 * 24 * 60 + b1 * 60 + c1) - (a2 * 24 * 60 + b2 * 60 + c2);
+    if(totalTime < 0)
     {
         cout << -1;
     }
     else
     {
-        while(true)
-        {
-            if(a1 == a2 && b1 == b2 && c1 == c2)
-                break;
-    
-            elapsed_time++;
-            c2++;
-        
-            if(c2 == 60) {
-                b2++;
-                c2 = 0;
-            }
-
-            if(b2 == 24){
-                a2++;
-                b2 = 0;
-            }
-        }
-        cout << elapsed_time;
+        cout << totalTime;
     }
     return 0;
 }
