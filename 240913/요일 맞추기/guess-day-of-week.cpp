7cba4274474b50ee9312b1e00cb0734cedf19be7
sindongmyung <41135138+sdm6410@users.noreply.github.com>
2024-09-13 17:51:08 +0900

    Title: 요일 맞추기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240913/요일 맞추기/guess-day-of-week.cpp
+++ 240913/요일 맞추기/guess-day-of-week.cpp
@@ -1,38 +1,59 @@
 #include <iostream>
 using namespace std;
-#define DAY 
-int main() {
-    int m1, d1, m2, d2;
-    int num_of_days[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    cin >> m1 >> d1 >> m2 >> d2;
 
-    string day[7] = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
-    int sum = 0;
-    for(int i = m1; i < m2; i++)
-    {
-        sum += num_of_days[i-1];
-    }
-    sum += d2;
-    sum -= d1;
-    if(m1 > m2)
-    {
-        sum = -sum;
-    }
+int days_of_months[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
+int cal(int m, int d) {
+    int month = 1, day = 1, elapsed_day = 0;
 
-    if(sum < 0)
-    {
-        while(sum < 0)
-        {
-            sum += 7;
+    while (true) {
+        if (month == m && day == d) {
+            break;
+        }
+        elapsed_day++;
+        day++;
+
+        if (day > days_of_months[month]) {
+            month++;
+            day = 1;
         }
     }
-    else
-    {
-        sum %= 7;
+    return elapsed_day;
+}
+
+int main() {
+    int m1, d1, m2, d2;
+    cin >> m1 >> d1 >> m2 >> d2;
+
+    int first = cal(m1, d1);
+    int second = cal(m2, d2);
+
+    int diff;
+    if (second > first) {
+        diff = second - first;
+        cout << diff << endl;
+        switch (diff % 7) {
+            case 0: cout << "Mon" << endl; break;
+            case 1: cout << "Tue" << endl; break;
+            case 2: cout << "Wed" << endl; break;
+            case 3: cout << "Thu" << endl; break;
+            case 4: cout << "Fri" << endl; break;
+            case 5: cout << "Sat" << endl; break;
+            case 6: cout << "Sun" << endl; break;
+        }
+    } else {
+        diff = first - second;
+        cout << diff << endl;
+        switch (diff % 7) {
+            case 0: cout << "Mon" << endl; break;
+            case 1: cout << "Sun" << endl; break;
+            case 2: cout << "Sat" << endl; break;
+            case 3: cout << "Fri" << endl; break;
+            case 4: cout << "Thu" << endl; break;
+            case 5: cout << "Wed" << endl; break;
+            case 6: cout << "Tue" << endl; break;
+        }
     }
 
-
-    cout << day[sum];
     return 0;
 }
