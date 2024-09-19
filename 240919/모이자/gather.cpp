229a881b58c44753a8c8093a8e0490e07328756b
sindongmyung <41135138+sdm6410@users.noreply.github.com>
2024-09-19 22:31:38 +0900

    Title: 모이자, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240919/모이자/gather.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int sum;
+    int min = 99999;
+    for(int i = 0; i < n; i++)
+    {
+        sum = 0;
+        for(int j = 1; j <= n; j++)
+        {
+            sum += arr[j-1] * abs(i + 1 - j);
+        }
+        if(sum < min)
+        {
+            min = sum;
+        }
+    }
+
+    cout << min;
+    return 0;
+}
