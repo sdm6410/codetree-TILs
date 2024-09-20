88903eb243e8a772b9e7d48476225f9614a7c628
sindongmyung <41135138+sdm6410@users.noreply.github.com>
2024-09-20 22:20:51 +0900

    Title: 일렬로 서있는 소 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240920/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int cnt = 0;
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = i + 1; j < n; j++)
+        {
+            for(int z = j + 1; z < n; z++)
+            {
+                if((arr[i] <= arr[j]) && (arr[j] <= arr[z]))
+                {
+                    cnt++;
+                }
+            }
+        }
+    }
+    cout << cnt;
+    return 0;
+}
