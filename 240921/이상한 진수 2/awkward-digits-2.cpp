138a13c3b34a8602911851fb6b5d9e30b4390773
sindongmyung <41135138+sdm6410@users.noreply.github.com>
2024-09-21 00:42:36 +0900

    Title: 이상한 진수 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240921/이상한 진수 2/awkward-digits-2.cpp
+++ 240921/이상한 진수 2/awkward-digits-2.cpp
@@ -1,43 +1,27 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
-#include <limits>
+#include <climits>
 
 using namespace std;
 
-int main() {
-    // 변수 선언 및 입력
-    string input;
-    cin >> input;
-    
-    int length = input.length();
-    vector<int> binary(length);
-    
-    for (int i = 0; i < length; ++i) {
-        binary[i] = input[i] - '0';  // Convert char to int
-    }
+int main()
+{
+    string bin;
+    cin >> bin;
 
-    // 각 i번째 자릿수를 바꾸었을 때의 십진수 값을 구해줍니다.
-    long long ans = numeric_limits<long long>::min();
-    
-    for (int i = 0; i < length; ++i) {
-        // i번째 자릿수를 바꿉니다.
-        binary[i] = 1 - binary[i];
-        
-        // 십진수로 변환합니다.
-        long long num = 0;
-        for (int j = 0; j < length; ++j) {
-            num = num * 2 + binary[j];
+    int ans = INT_MIN;
+    for(int i = 0; i < (int)bin.size(); i++)
+    {
+        bin[i] = '0' + '1' - bin[i];
+        int num = 0;
+        for(int j = 0; j < (int)bin.size(); j++)
+        {
+            num = num * 2 + (bin[j] - '0');
         }
-        
+
         ans = max(ans, num);
-        
-        // i번째 자릿수를 원래대로 돌려놓습니다.
-        binary[i] = 1 - binary[i];
+
+        bin[i] = '0' + '1' - bin[i];
     }
+    cout << ans;
 
-    // 출력
-    cout << ans << endl;
-
-    return 0;
 }
