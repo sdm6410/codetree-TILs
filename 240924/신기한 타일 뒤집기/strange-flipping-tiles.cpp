583f6a21fc9e15886ba117769237682587a17906
sindongmyung <41135138+sdm6410@users.noreply.github.com>
2024-09-24 23:44:51 +0900

    Title: 신기한 타일 뒤집기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240924/신기한 타일 뒤집기/strange-flipping-tiles.cpp
+++ 240924/신기한 타일 뒤집기/strange-flipping-tiles.cpp
@@ -1,33 +1,45 @@
 #include <iostream>
-#define MAX 100000
+#define MAX_K 100000
 using namespace std;
-int n ,cur = 0;
-int x1[MAX * 2 + 1] = {0,};
-int main() {
-    cin>> n;
-    for(int i = 0; i < n; i++){
+
+int n;
+int a[2 * MAX_K + 1];
+int b, w;
+
+int main()
+{
+    cin >> n;
+
+    int cur = MAX_K;
+    for(int i = 1; i <= n; i++)
+    {
         int x;
-        char direction;
-        cin>> x>> direction;
-        if(direction=='L'){
-            while(x--){
-                x1[cur + MAX] = 1;
+        char c;
+        cin >> x >> c;
+        if(c == 'L')
+        {
+            while(x--)
+            {
+                a[cur] = 1;
                 if(x) cur--;
-                //cout<<cur<<'a'<<endl;
             }
-        }else{
-            while(x--){
-                x1[cur + MAX] = 2;
+        }
+        else
+        {
+            while(x--)
+            {
+                a[cur] = 2;
                 if(x) cur++;
-                //cout<<cur<<'a'<<endl;
             }
         }
     }
-    int w = 0, b = 0;
-    for(int i = 0; i < MAX * 2 + 1; i++){
-        if(x1[i] == 1) w++;
-        else if(x1[i] == 2) b++;
+
+    for(int i = 0; i <= 2 * MAX_K; i++)
+    {
+        if(a[i] == 1) w++;
+        if(a[i] == 2) b++;
     }
-    cout<< w<< " "<< b;
+
+    cout << w << " " << b;
     return 0;
 }
