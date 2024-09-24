c4b2b12323ec266282c558829a11f43811c84d14
sindongmyung <41135138+sdm6410@users.noreply.github.com>
2024-09-24 16:53:36 +0900

    Title: 왔다 갔던 구역 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240924/왔다 갔던 구역 2/area-been-to-and-from2.cpp
+++ 240924/왔다 갔던 구역 2/area-been-to-and-from2.cpp
@@ -1,49 +1,58 @@
 #include <iostream>
-#define MAX_R 1000
+
+#define MAX_N 100
+#define MAX_R 2000
+#define OFFSET 1000
+
 using namespace std;
 
-int checked[202];
+int n;
+int x1[MAX_N], x2[MAX_N];
 
-int main() {
-    int n;
+int checked[MAX_R + 1];
+
+int main()
+{
     cin >> n;
-    int point = 101;
+
+    int cur = 0;
+
     for(int i = 0; i < n; i++)
     {
-        int x;
-        char c;
-        cin >> x >> c;
+        int distance;
+        char direction;
+        cin >> distance >> direction;
 
-        if(c == 'R')
+        if(direction == 'L')
         {
-            for(int j = 0; j < x; j++)
-            {
-                checked[point + j]++;
-            }
-
-            point += x;
+            x1[i] = cur - distance;
+            x2[i] = cur;
+            cur -= distance;
         }
-
-        if(c == 'L')
+        else
         {
-            for(int j = 1; j <= x; j++)
-            {
-                checked[point - j]++;
-            }
-            point -= x;
+            x1[i] = cur;
+            x2[i] = cur + distance;
+            cur += distance;
         }
-        
+        x1[i] += OFFSET;
+        x2[i] += OFFSET;
+    }
+
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = x1[i]; j < x2[i]; j++)
+        {
+            checked[j]++;
+        }
     }
 
     int cnt = 0;
-    for(int i = 0; i < 202; i++)
+    for(int i = 0; i <= MAX_R; i++)
     {
         if(checked[i] >= 2)
-        {
             cnt++;
-        }
     }
-
     cout << cnt;
     return 0;
 }
