63c2f70dd8d86027c77cd7f0133341792f1b9aad
sindongmyung <41135138+sdm6410@users.noreply.github.com>
2024-09-29 21:34:26 +0900

    Title: 1이 3개 이상 있는 위치, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240929/1이 3개 이상 있는 위치/place-more-than-3-ones.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[n][n];
+    int answer = 0;
+    int dx[4] = {1, -1, 0, 0};
+    int dy[4] = {0, 0, -1, 1};
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+    int x = 0;
+    int y = 0; 
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            int cnt = 0;
+            for(int z = 0; z < 4; z++)
+            {
+
+                x = i + dx[z];
+                y = j + dy[z];
+                if(arr[x][y] == 1)
+                {
+                    cnt++;
+                }
+            }
+
+            if(cnt >= 3)
+            {
+                answer++;
+            }
+        }
+    }
+    cout << answer;
+    return 0;
+}
