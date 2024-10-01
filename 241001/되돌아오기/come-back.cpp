47bdc1c79f4162dbfe9f6c59345ec97b212b7221
sindongmyung <41135138+sdm6410@users.noreply.github.com>
2024-10-01 22:19:20 +0900

    Title: 되돌아오기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241001/되돌아오기/come-back.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+int GetDir(char c)
+{
+    if(c == 'W')
+    {
+        return 0;
+    }
+    else if(c == 'S')
+    {
+        return 1;
+    }
+    else if(c == 'N')
+    {
+        return 2;
+    }
+    else
+    {
+        return 3;
+    }
+}
+int main() {
+    int n;
+    cin >> n;
+    int x = 0, y = 0;
+    int dx[4] = {-1, 0, 0, 1};
+    int dy[4] = {0, -1, 1, 0};
+    int cnt = 0;
+    for(int i = 0; i < n; i++)
+    {
+        char d;
+        int s;
+        cin >> d >> s;
+        int dir = GetDir(d);
+        bool flag = false;
+        for(int j = 0; j < s; j++)
+        {
+            x = x + dx[dir];
+            y = y + dy[dir];
+            cnt++;
+            if(x == 0 && y == 0)
+            {
+                flag = true;
+                break;
+            }
+        }
+
+        if(flag)
+            break;
+    }
+    cout << cnt;
+    return 0;
+}
