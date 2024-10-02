56a98f9ab69cb5724348589aef0390e619dd942f
sindongmyung <41135138+sdm6410@users.noreply.github.com>
2024-10-02 09:15:01 +0900

    Title: 좌우로 움직이는 로봇, Time: 45ms, Memory: 8MB, Status: Passed - CodeTree

--- 241002/좌우로 움직이는 로봇/robot-moving-from-side-to-side.cpp
+++ 241002/좌우로 움직이는 로봇/robot-moving-from-side-to-side.cpp
@@ -1,55 +1,90 @@
 #include <iostream>
 #include <vector>
-#include <algorithm> // for max function
+#include <algorithm>
+
 using namespace std;
 
 int main() {
-    int N, M;
-    cin >> N >> M;
+    int n, m;
+    cin >> n >> m; // a의 움직임, b의 움직임
 
-    // 입력 최대치에 따라 배열 크기를 넉넉하게 설정
-    vector<int> pos_a(100001, 0), pos_b(100001, 0);
-    int time_a = 1, time_b = 1;
+    vector<int> timeA(n), timeB(m);
+    vector<char> distA(n), distB(m);
 
-    // t만큼의 변화동안 pos_a
-    for (int i = 0; i < N; i++) {
-        int t;
-        char d;
-        cin >> t >> d;
-        for (int j = 0; j < t; j++) {
-            pos_a[time_a] = pos_a[time_a - 1] + (d == 'R' ? 1 : -1);
-            time_a++;
+    int timesizeA = 0; // a의 총 이동 시간
+    for (int i = 0; i < n; ++i) {
+        int time;
+        cin >> time;
+        timesizeA += time;
+        timeA[i] = time;
+        cin >> distA[i]; // 방향 입력
+    }
+
+    int timesizeB = 0; // b의 총 이동 시간
+    for (int i = 0; i < m; ++i) {
+        int time;
+        cin >> time;
+        timesizeB += time;
+        timeB[i] = time;
+        cin >> distB[i]; // 방향 입력
+    }
+
+    int size = max(timesizeA, timesizeB); // 더 큰 사이즈 선택
+    vector<int> locationA(size + 1, 0), locationB(size + 1, 0);
+
+    int location = 0;
+    int idx = 0; // 다음 시간의 인덱스
+    for (int i = 0; i < n; ++i) {
+        if (distA[i] == 'R') {
+            for (int j = 1; j <= timeA[i]; ++j) {
+                location++;
+                locationA[idx + j] = location;
+            }
+        } else {
+            for (int j = 1; j <= timeA[i]; ++j) {
+                location--;
+                locationA[idx + j] = location;
+            }
+        }
+        idx += timeA[i]; // 진행한 시간만큼 인덱스 증가
+    }
+
+    location = 0;
+    idx = 0;
+    for (int i = 0; i < m; ++i) {
+        if (distB[i] == 'R') {
+            for (int j = 1; j <= timeB[i]; ++j) {
+                location++;
+                locationB[idx + j] = location;
+            }
+        } else {
+            for (int j = 1; j <= timeB[i]; ++j) {
+                location--;
+                locationB[idx + j] = location;
+            }
+        }
+        idx += timeB[i];
+    }
+
+    // 먼저 종료된 경우 마지막 위치로 채우기
+    int min_time = min(timesizeA, timesizeB);
+    if (timesizeA < timesizeB) {
+        for (int i = min_time + 1; i <= size; ++i) {
+            int last = locationA[min_time];
+            locationA[i] = last;
+        }
+    } 
+    if (timesizeA > timesizeB) {
+        for (int i = min_time + 1; i <= size; ++i) {
+            int last = locationB[min_time];
+            locationB[i] = last;
         }
     }
 
-    // t만큼의 변화동안 pos_b
-    for (int i = 0; i < M; i++) {
-        int t;
-        char d;
-        cin >> t >> d;
-        for (int j = 0; j < t; j++) {
-            pos_b[time_b] = pos_b[time_b - 1] + (d == 'R' ? 1 : -1);
-            time_b++;
-        }
-    }
-
-    // time_a, time_b가 다른 경우, 차이만큼 동안의 위치 보정
-    if (time_a < time_b) {
-        for (int i = time_a; i < time_b; ++i) {
-            pos_a[i] = pos_a[i - 1];
-        }
-    } else if (time_a > time_b) {
-        for (int i = time_b; i < time_a; ++i) {
-            pos_b[i] = pos_b[i - 1];
-        }
-    }
-
+    // 두 위치가 같아지는 경우의 수 카운트
     int cnt = 0;
-    int time_max = max(time_a, time_b);
-    
-    // 위치가 같은 지점에서 상태가 바뀌었는지 확인
-    for (int i = 1; i < time_max; ++i) {
-        if (pos_a[i] == pos_b[i] && pos_a[i - 1] != pos_b[i - 1]) {
+    for (int i = 1; i <= size; ++i) {
+        if (locationA[i] == locationB[i] && locationA[i - 1] != locationB[i - 1]) {
             cnt++;
         }
     }
