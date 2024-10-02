a21c63acbfd1e45d071f951202563eb96dac94da
sindongmyung <41135138+sdm6410@users.noreply.github.com>
2024-10-02 09:09:41 +0900

    Title: 좌우로 움직이는 로봇, Time: 16ms, Memory: 1MB, Status: Runtime Error - CodeTree

--- 241002/좌우로 움직이는 로봇/robot-moving-from-side-to-side.cpp
+++ 241002/좌우로 움직이는 로봇/robot-moving-from-side-to-side.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <vector>
+#include <algorithm> // for max function
 using namespace std;
 
 int main() {
     int N, M;
     cin >> N >> M;
 
-    // time에 따른 a, b의 위치 기록할 수 있는 배열 생성
-    vector<int> pos_a(100000, 0), pos_b(100000, 0);
+    // 입력 최대치에 따라 배열 크기를 넉넉하게 설정
+    vector<int> pos_a(200001, 0), pos_b(200001, 0);
     int time_a = 1, time_b = 1;
 
     // t만큼의 변화동안 pos_a
@@ -45,6 +46,8 @@ int main() {
 
     int cnt = 0;
     int time_max = max(time_a, time_b);
+    
+    // 위치가 같은 지점에서 상태가 바뀌었는지 확인
     for (int i = 1; i < time_max; ++i) {
         if (pos_a[i] == pos_b[i] && pos_a[i - 1] != pos_b[i - 1]) {
             cnt++;
