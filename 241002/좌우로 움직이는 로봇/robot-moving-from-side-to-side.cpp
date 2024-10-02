2c91c93644d5ee958580b169e51d4a6db77c2da1
sindongmyung <41135138+sdm6410@users.noreply.github.com>
2024-10-02 09:07:46 +0900

    Title: 좌우로 움직이는 로봇, Time: 17ms, Memory: 0MB, Status: Runtime Error - CodeTree

--- 241002/좌우로 움직이는 로봇/robot-moving-from-side-to-side.cpp
+++ 241002/좌우로 움직이는 로봇/robot-moving-from-side-to-side.cpp
@@ -1,53 +1,57 @@
 #include <iostream>
-#define MAX_T 1000000
+#include <vector>
 using namespace std;
-int n, m;
-int pos_a[MAX_T + 1], pos_b[MAX_T + 1];
+
 int main() {
-    cin >> n >> m;
-    int time_a = 1;
-    for(int i = 0; i < n; i++)
-    {
-        int nA;
-        char cDirA;
-        cin >> nA >> cDirA;
-        while(nA--)
-        {
-            if(cDirA == 'R')
-                pos_a[time_a] = pos_a[time_a -1] + 1;
-            else
-                pos_a[time_a] = pos_a[time_a -1] - 1;
+    int N, M;
+    cin >> N >> M;
+
+    // time에 따른 a, b의 위치 기록할 수 있는 배열 생성
+    vector<int> pos_a(100000, 0), pos_b(100000, 0);
+    int time_a = 1, time_b = 1;
+
+    // t만큼의 변화동안 pos_a
+    for (int i = 0; i < N; ++i) {
+        int t;
+        char d;
+        cin >> t >> d;
+        for (int j = 0; j < t; ++j) {
+            pos_a[time_a] = pos_a[time_a - 1] + (d == 'R' ? 1 : -1);
             time_a++;
         }
     }
 
-    int time_b = 1;
-    for(int i = 0; i < m; i++)
-    {
-        int nB;
-        char cDirB;
-        cin >> nB >> cDirB;
-        while(nB--)
-        {
-            if(cDirB == 'R')
-            {
-                pos_b[time_b] = pos_b[time_b - 1] + 1;
-            }
-            else
-                pos_b[time_b] = pos_b[time_b - 1] - 1;
+    // t만큼의 변화동안 pos_b
+    for (int i = 0; i < M; ++i) {
+        int t;
+        char d;
+        cin >> t >> d;
+        for (int j = 0; j < t; ++j) {
+            pos_b[time_b] = pos_b[time_b - 1] + (d == 'R' ? 1 : -1);
             time_b++;
         }
     }
 
-    int ans = -1;
-    for(int i = 1; i < time_b; i++)
-    {
-        if(pos_a[i] == pos_b[i])
-        {
-            ans++;
+    // time_a, time_b가 다른 경우, 차이만큼 동안의 위치 보정
+    if (time_a < time_b) {
+        for (int i = time_a; i < time_b; ++i) {
+            pos_a[i] = pos_a[i - 1];
+        }
+    } else if (time_a > time_b) {
+        for (int i = time_b; i < time_a; ++i) {
+            pos_b[i] = pos_b[i - 1];
         }
     }
 
-    cout << ans;
+    int cnt = 0;
+    int time_max = max(time_a, time_b);
+    for (int i = 1; i < time_max; ++i) {
+        if (pos_a[i] == pos_b[i] && pos_a[i - 1] != pos_b[i - 1]) {
+            cnt++;
+        }
+    }
+
+    cout << cnt << endl;
+
     return 0;
 }
