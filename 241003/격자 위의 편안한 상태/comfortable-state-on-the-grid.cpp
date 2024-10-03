43684b75de33951f4d0e2e4e40ce16361c0fe866
sindongmyung <41135138+sdm6410@users.noreply.github.com>
2024-10-03 21:15:45 +0900

    Title: 격자 위의 편안한 상태, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241003/격자 위의 편안한 상태/comfortable-state-on-the-grid.cpp
+++ 241003/격자 위의 편안한 상태/comfortable-state-on-the-grid.cpp
@@ -5,7 +5,7 @@ int dx[4] = {-1, 1, 0, 0};
 int dy[4] = {0, 0, -1, 1};
 
 bool InRange(int x, int y){
-    return 0 <= x && x < n && 0 <= y && y < m;
+    return 0 <= x && x < n && 0 <= y && y < n;
 }
 
 
