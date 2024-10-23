6040e16073a69fef256ee8e50fc2dc16997b6c6d
sindongmyung <41135138+sdm6410@users.noreply.github.com>
2024-10-23 11:56:45 +0900

    Title: 스승의 은혜 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241023/스승의 은혜 2/the-grace-form-teacher-2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n, b;
+
+int main() {
+    cin >> n >> b;
+    int arr[n];
+    for(int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    sort(arr, arr + n);
+    int cnt = 0;
+    for(int i = 0; i < n; i++)
+    {
+        if(b < 0)
+            break;
+        b -= arr[i];
+        cnt++;
+    }
+
+    cout << cnt;
+    return 0;
+}
