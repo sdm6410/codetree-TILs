9314c39ae50231794b9e982e7e740b906d2c1a77
sindongmyung <41135138+sdm6410@users.noreply.github.com>
2024-10-23 09:39:34 +0900

    Title: 개발자의 능력 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241023/개발자의 능력 2/ability-of-developer-2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include<algorithm>
+using namespace std;
+
+int main() {
+    int arr[6];
+    for(int i = 0; i < 6; i++)
+    {
+        cin >> arr[i];
+    }
+    sort(arr, arr + 6);
+
+    int max_ = 0;
+    int min_ = 999999;
+    for(int i = 0; i < 3; i++)
+    {
+        int num = arr[i] + arr[5 - i];
+        if(max_ < num)
+            max_ = num;
+        
+        if(min_ > num)
+            min_ = num;
+    }
+
+    cout << max_ - min_;
+    return 0;
+}
