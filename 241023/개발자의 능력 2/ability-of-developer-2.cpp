c04b81c5ba07f73ad2e78584de65f56e5764ea7a
sindongmyung <41135138+sdm6410@users.noreply.github.com>
2024-10-23 09:40:25 +0900

    Title: 개발자의 능력 2, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241023/개발자의 능력 2/ability-of-developer-2.cpp
+++ 241023/개발자의 능력 2/ability-of-developer-2.cpp
@@ -11,7 +11,7 @@ int main() {
     sort(arr, arr + 6);
 
     int max_ = 0;
-    int min_ = 1000000;
+    int min_ = 886606;
     for(int i = 0; i < 3; i++)
     {
         int num = arr[i] + arr[5 - i];
