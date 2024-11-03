8fa53b24956869927f3a081807651d35f53e50cc
sindongmyung <41135138+sdm6410@users.noreply.github.com>
2024-11-03 15:50:24 +0900

    Title: 3개의 선 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241103/3개의 선 2/three-lines-2.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+#define MAX_N 20
+int n;
+int arr_x[MAX_N], arr_y[MAX_N];
+
+bool findPoint(int x, int y, int z)
+{
+    int cnt = 0;
+    for(int i = 0; i < n; i++)
+    {
+        if(arr_x[i] == x || arr_y[i] == y || arr_x[i] == z || arr_y[i] == z)
+        {
+            cnt++;
+        }
+    }
+    if(cnt == n)
+        return true;
+    return false;
+}
+
+int main() {
+    int ans = 0;
+    cin >> n;
+    for(int i = 0; i < n; i++)
+    {
+        cin >> arr_x[i] >> arr_y[i];
+    }
+
+    for(int i = 0; i <= 10; i++)
+    {
+        for(int j = 0; j <= 10; j++)
+        {
+            for(int z = j+1; z<= 10; z++)
+            {
+                if(findPoint(i, j, z))
+                    ans = 1;
+            }
+        }
+    }
+
+    cout << ans;
+    return 0;
+}
