e1b466cd94e49f88d6cd0403449a36c71cb7e5da
sindongmyung <41135138+sdm6410@users.noreply.github.com>
2024-11-03 16:04:39 +0900

    Title: 3개의 선 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 241103/3개의 선 2/three-lines-2.cpp
+++ 241103/3개의 선 2/three-lines-2.cpp
@@ -1,44 +1,107 @@
 #include <iostream>
-using namespace std;
-#define MAX_N 20
-int n;
-int arr_x[MAX_N], arr_y[MAX_N];
+#include <vector>
+#include <algorithm>
 
-bool findPoint(int x, int y, int z)
-{
-    int cnt = 0;
-    for(int i = 0; i < n; i++)
-    {
-        if(arr_x[i] == x || arr_y[i] == x || arr_x[i] == y || arr_y[i] == y || arr_x[i] == z || arr_y[i] == z)
-        {
-            cnt++;
+using namespace std;
+
+// 전역 변수
+int n;
+vector<pair<int, int>> points;
+
+// vertical 함수: x좌표가 k인 점들을 반환
+vector<pair<int, int>> vertical(int k) {
+    vector<pair<int, int>> curr_line;
+    for (auto [x, y] : points) {
+        if (x == k) {
+            curr_line.push_back({x, y});
         }
     }
-    if(cnt == n)
-        return true;
-    return false;
+    return curr_line;
+}
+
+// parallel 함수: y좌표가 k인 점들을 반환
+vector<pair<int, int>> parallel(int k) {
+    vector<pair<int, int>> curr_line;
+    for (auto [x, y] : points) {
+        if (y == k) {
+            curr_line.push_back({x, y});
+        }
+    }
+    return curr_line;
+}
+
+// search 함수: 세 개의 선분이 포함하는 고유한 점의 수를 계산
+int search(int a, int b, int c, const vector<vector<pair<int, int>>>& points_belongs_to_line) {
+    vector<pair<int, int>> line_1 = points_belongs_to_line[a];
+    vector<pair<int, int>> line_2 = points_belongs_to_line[b];
+    vector<pair<int, int>> line_3 = points_belongs_to_line[c];
+
+    vector<pair<int, int>> curr_points_belongs_to_three_lines;
+
+    // 첫번째 선분에서 중복되지 않는 점 추가
+    for (auto point : line_1) {
+        if (find(curr_points_belongs_to_three_lines.begin(), curr_points_belongs_to_three_lines.end(), point) == curr_points_belongs_to_three_lines.end()) {
+            curr_points_belongs_to_three_lines.push_back(point);
+        }
+    }
+
+    // 두번째 선분에서 중복되지 않는 점 추가
+    for (auto point : line_2) {
+        if (find(curr_points_belongs_to_three_lines.begin(), curr_points_belongs_to_three_lines.end(), point) == curr_points_belongs_to_three_lines.end()) {
+            curr_points_belongs_to_three_lines.push_back(point);
+        }
+    }
+
+    // 세번째 선분에서 중복되지 않는 점 추가
+    for (auto point : line_3) {
+        if (find(curr_points_belongs_to_three_lines.begin(), curr_points_belongs_to_three_lines.end(), point) == curr_points_belongs_to_three_lines.end()) {
+            curr_points_belongs_to_three_lines.push_back(point);
+        }
+    }
+
+    // 고유한 점의 개수 반환
+    return curr_points_belongs_to_three_lines.size();
 }
 
 int main() {
-    int ans = 0;
+    // n 입력
     cin >> n;
-    for(int i = 0; i < n; i++)
-    {
-        cin >> arr_x[i] >> arr_y[i];
+    
+    // points 입력
+    points.resize(n);
+    for (int i = 0; i < n; i++) {
+        int x, y;
+        cin >> x >> y;
+        points[i] = {x, y};
     }
 
-    for(int i = 0; i <= 10; i++)
-    {
-        for(int j = 0; j <= 10; j++)
-        {
-            for(int z = 0; z <= 10; z++)
-            {
-                if(findPoint(i, j, z))
-                    ans = 1;
+    // points_belongs_to_line 초기화
+    vector<vector<pair<int, int>>> points_belongs_to_line;
+
+    // 모든 수직/수평 선분 조사
+    for (int i = 0; i < 10; i++) {
+        points_belongs_to_line.push_back(vertical(i));  // 수직 선분
+        points_belongs_to_line.push_back(parallel(i));  // 수평 선분
+    }
+
+    // max_point_cnt 초기화
+    int max_point_cnt = 0;
+
+    // 선분 3개를 고르기
+    for (int i = 0; i < 8; i++) {
+        for (int j = i + 1; j < 9; j++) {
+            for (int k = j + 1; k < 10; k++) {
+                max_point_cnt = max(max_point_cnt, search(i, j, k, points_belongs_to_line));
             }
         }
     }
 
-    cout << ans;
+    // 결과 출력
+    if (max_point_cnt == n) {
+        cout << 1 << endl;
+    } else {
+        cout << 0 << endl;
+    }
+
     return 0;
 }
