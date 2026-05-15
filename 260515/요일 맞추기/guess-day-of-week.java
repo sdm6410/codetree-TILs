594954c9ec854e5ddb07ac642da134dd0d333dd5
sindongmyung <41135138+sdm6410@users.noreply.github.com>
2026-05-15 16:30:22 +0900

    [Passed] 요일 맞추기 (128ms, 10MB)

---
+++ 260515/요일 맞추기/guess-day-of-week.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class Main {
+
+    public static int numOfDays(int m, int d)
+    {
+        int[] days = new int[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int totalDays = 0;
+        for(int i = 1; i < m; i++)
+        {
+            totalDays += days[i];
+        }
+        totalDays += d;
+
+        return totalDays;
+    }
+
+
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int m1 = sc.nextInt();
+        int d1 = sc.nextInt();
+        int m2 = sc.nextInt();
+        int d2 = sc.nextInt();
+
+        int diff = numOfDays(m2, d2) - numOfDays(m1, d1);
+
+        while(diff < 0)
+            diff += 7;
+
+        String[] dayOfWeek = new String[]{"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+        System.out.print(dayOfWeek[diff % 7]);
+    }
+}
